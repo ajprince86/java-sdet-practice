1b03e0eef9dbb475918d6708180690c5b3af3068
Amari Princivil <ajthacode88@gmail.com>
2026-06-08 13:17:37 -0400

    Added invalid login flow

--- src/test/java/com/sdet/practice/LoginTest.java
+++ src/test/java/com/sdet/practice/LoginTest.java
@@ -35,6 +35,19 @@ public class LoginTest {
 
     }
 
+    @Test
+    public void testInvalidLogin(){
+
+        driver.findElement(By.id("user-name")).sendKeys("wrong_user");
+
+        driver.findElement(By.id("password")).sendKeys("invalid_password");
+
+        driver.findElement((By.id("login-button"))).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("[data-test='error']")).getText(),"Epic sadface: Username and password do not match any user in this service");
+
+    }
+
     @AfterMethod
     public void closeDriver(){
 
